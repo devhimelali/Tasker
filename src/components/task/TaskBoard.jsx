7cd69c204cd8d58d313d580b39dec2a822ef585b
devhimelali <dev.himelali@gmail.com>
2025-05-16 01:01:14 +0600

    Extract UI elements into separate component for better reusability

---
+++ src/components/task/TaskBoard.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import SearchBox from "./SearchBox";
+import TaskHeader from "./TaskHeader";
+import TaskList from "./TaskList";
+
+export default function TaskBoard() {
+  const [tasks, setTasks] = useState([]);
+  return (
+    <section className="mb-20" id="tasks">
+      <div className="container">
+        <div className="p-2 flex justify-end">
+          <SearchBox />
+        </div>
+        <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
+          <TaskHeader />
+          <TaskList />
+        </div>
+      </div>
+    </section>
+  );
+}
